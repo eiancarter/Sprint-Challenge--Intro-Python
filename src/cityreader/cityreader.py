fd9c4ebb13abea68e3fa716207fec0d0a176625a
Eian Carter <eian@bricleir.com>
2020-05-08 10:23:03 -0700

    corrected error reading csv needed to make lat lon rows floats

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,11 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
-
+import csv
+class City:
+      def __init__(self, name, lat, lon):
+            self.name = name
+            self.lat = lat
+            self.lon = lon
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -20,8 +25,13 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+  with open('cities.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    # line_count = 0
+    for row in csv_reader:
+      city = City(row["city"], float(row["lat"]), float(row["lng"]))
+      cities.append(city)
+  return cities
 
 cityreader(cities)
 
