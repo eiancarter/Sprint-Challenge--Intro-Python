22459605600668496d5566ba209501d29fd01986
Eian Carter <eian@bricleir.com>
2020-05-08 08:30:33 -0700

    completed oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,33 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle is the base class -
+
+class Vehicle:
+    def __init__(self, type):
+        self.type = type
+
+class GroundVehicle(Vehicle):
+    def __init__(self, type, ground_type):
+        super().__init__(type)
+        self.ground_type = ground_type
+
+class FlightVehicle(Vehicle):
+    def __init__(self, type, air_type):
+        super().__init__(type)
+        self.air_type = air_type
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+class StarShip(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+    
+    
